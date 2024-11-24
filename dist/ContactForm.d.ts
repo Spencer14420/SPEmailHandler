dc46c68d0c958cbe0e38142fd7c7ca9982ee8eaa
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-11-24 14:08:00 -0500

    Allow users to set a callback function (#11)
    * Allow users to set a callback function
    
    * 1.3.0

--- dist/ContactForm.d.ts
+++ dist/ContactForm.d.ts
@@ -1,15 +1,19 @@
 import "bootstrap";
 import { Modal } from "bootstrap";
+
 export declare class ContactForm {
-    serverScript: string;
-    tokenInputName: string | null;
-    successModal: Modal | null;
-    messageAlert: HTMLElement | null;
-    constructor(serverScript: string, tokenInputName?: string | null);
-    initializeEventListeners(): void;
-    isEmail(email: string): boolean;
-    messageSuccess(): void;
-    sendMessage(data: Record<string, string>): Promise<void>;
-    handleSubmit(): void;
-    displayAlert(message: string): void;
+  serverScript: string;
+  tokenInputName: string | null;
+  successModal: Modal | null;
+  messageAlert: HTMLElement | null;
+  onSuccess: ((responseData: Record<string, any>) => void) | null;
+
+  constructor(
+    serverScript: string,
+    tokenInputName?: string | null,
+    onSuccess?: ((responseData: Record<string, any>) => void) | null,
+  );
+
+  handleSubmit(): void;
+  displayAlert(message: string): void;
 }
